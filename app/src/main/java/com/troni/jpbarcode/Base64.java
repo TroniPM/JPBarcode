2d9333d52dd607f003de59ed02d30e27badecc80
Mateus <paulomatew@gmail.com>
2016-12-06 17:42:09 -0300

    credits

--- app/src/main/java/com/troni/jpbarcode/Base64.java
+++ app/src/main/java/com/troni/jpbarcode/Base64.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.troni.jpbarcode;
 
 import java.io.ByteArrayOutputStream;
 
 /**
  *
- * @author Mateus
+ * @author EmilHernvall https://gist.github.com/EmilHernvall/953733
  */
 public class Base64 {
 
